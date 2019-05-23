f2a5f9b0510c38950e8413800cc2bad7184049b4
Shane678 <shanewestlife@outlook.com>
2019-05-23 20:39:39 +0800

    异常类型_ArrayIndexOutOfBoundsException_edit

--- kgc/Java面向对象/7异常/ExceptionDemos/src/Test0.java
+++ kgc/Java面向对象/7异常/ExceptionDemos/src/Test0.java
@@ -10,11 +10,19 @@ public class Test0 {
 		int num2 = in.nextInt();
 		System.out.println(num1 + "/" + num2 + "=" + num1 / num2);
 		System.out.println("感谢使用本程序！");*/
-		int[] nums = new int[2];
-		System.out.println(nums[7]);
+		try{
+			int[] nums = new int[2];
+			System.out.println(nums[7]);
+		}catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+			System.out.println("*******");
+			System.out.println(e.getMessage());
+		}
 //		运行结果：
-//			Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 7
-//			at Test0.main(Test0.java:14)
-//		表示数组下标越界
+//			java.lang.ArrayIndexOutOfBoundsException: 7
+//				at Test0.main(Test0.java:15)
+//			*******
+//			7
+//		可以看到，getMessage()获得的是冒号后面的内容。
 	}
 }
