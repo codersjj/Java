471513fe62ce61d5715faeb0ffc3f5673aa21237
Shane678 <shanewestlife@outlook.com>
2019-05-23 16:18:18 +0800

    使用try-catch处理异常_情况2

--- kgc/Java面向对象/7异常/ExceptionDemos/src/Test2.java
+++ kgc/Java面向对象/7异常/ExceptionDemos/src/Test2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 //使用try-catch处理异常
 //第一种情况：try部分的代码正常（不会产生异常），程序运行时执行完try部分后会跳过catch部分的代码，直接执行后面的代码。
+//第二种情况：try部分的代码出现异常（比如下面的代码）
 public class Test2 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -19,3 +20,9 @@ public class Test2 {
 		System.out.println("感谢使用本程序！");
 	}
 }
+
+// java中所有的异常都是Exception（父类）
+// 除数为0的异常：ArithmeticException（子类）
+// 输入的格式不正确：InputMismatchException（子类）
+// catch后面的类型如果是Exception类型，优点是可以抓住所有异常，缺点是抓住的异常不具体、不精准。
+
