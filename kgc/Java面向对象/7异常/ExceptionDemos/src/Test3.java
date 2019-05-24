09b5be4074b35d06a49d1bc9c4374c81a47bd3a5
Shane678 <shanewestlife@outlook.com>
2019-05-24 10:39:32 +0800

    try-catch-finally_finally不执行的唯一情况

--- kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
+++ kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
@@ -14,16 +14,22 @@ public class Test3 {
 		}catch(InputMismatchException e){ 
 			System.err.println("出现了错误操作！");
 			e.printStackTrace();
+			// 退出JVM   exit()只要传一个非零的int类型值即可
+			System.exit(7);
 		}finally{
 			System.out.println("感谢使用本程序！");
 		}
 	}
 }
 
-//	加上finally后，不管发不发生异常，程序都会执行finally块。不执行的唯一情况是在finally块前退出了JVM。
+//	加上finally后不执行的唯一情况是在finally块前退出了JVM。
 //	运行结果：
 //		请输入被除数：100
-//		请输入除数：0
-//		感谢使用本程序！
-//		Exception in thread "main" java.lang.ArithmeticException: / by zero
-//			at Test3.main(Test3.java:13)
+//		请输入除数：q
+//		出现了错误操作！
+//		java.util.InputMismatchException
+//			at java.util.Scanner.throwFor(Scanner.java:909)
+//			at java.util.Scanner.next(Scanner.java:1530)
+//			at java.util.Scanner.nextInt(Scanner.java:2160)
+//			at java.util.Scanner.nextInt(Scanner.java:2119)
+//			at Test3.main(Test3.java:12)
