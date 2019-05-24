d707947c55c44d9963fc99a81f3682a1afc87dc3
Shane678 <shanewestlife@outlook.com>
2019-05-24 11:13:07 +0800

    try块中有return

--- kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
+++ kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
@@ -11,6 +11,7 @@ public class Test3 {
 			System.out.print("请输入除数：");
 			int num2 = in.nextInt();
 			System.out.println(num1 + "/" + num2 + "=" + num1 / num2);
+			return;
 		}catch(InputMismatchException e){ 
 			System.err.println("出现了错误操作！");
 			e.printStackTrace();
@@ -23,15 +24,11 @@ public class Test3 {
 	}
 }
 
-//	catch块中若有return，执行到return时不会直接退出方法，而会先执行finally，之后再执行return，退出方法。
+//	对于try-catch-finally，try块中若有return，执行到return时不会直接退出方法，而会先执行finally，之后再执行return，退出方法。
 //	运行结果：
-//		请输入被除数：100
-//		请输入除数：b
-//		出现了错误操作！
-//		java.util.InputMismatchException
-//			at java.util.Scanner.throwFor(Scanner.java:909)
-//			at java.util.Scanner.next(Scanner.java:1530)
-//			at java.util.Scanner.nextInt(Scanner.java:2160)
-//			at java.util.Scanner.nextInt(Scanner.java:2119)
-//			at Test3.main(Test3.java:12)
+//		请输入被除数：7
+//		请输入除数：2
+//		7/2=3
 //		感谢使用本程序！
+//	总结：不管return出现在try块中还是catch块中，遇到return时，都会先去执行finally块，再来执行return跳出方法。
+//		只有一种情况下不会执行finally，即System.exit(1)退出Java虚拟机。
