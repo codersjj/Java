be6d608768b76551c7b7aac7024f2cd8b89cdbab
Shane678 <shanewestlife@outlook.com>
2019-05-24 11:49:01 +0800

    多重catch

--- kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
+++ kgc/Java面向对象/7异常/ExceptionDemos/src/Test3.java
@@ -11,24 +11,25 @@ public class Test3 {
 			System.out.print("请输入除数：");
 			int num2 = in.nextInt();
 			System.out.println(num1 + "/" + num2 + "=" + num1 / num2);
-			return;
+//			return;
 		}catch(InputMismatchException e){ 
-			System.err.println("出现了错误操作！");
+			System.err.println("输入必须为整数！");
 			e.printStackTrace();
 			// 退出JVM   exit()只要传一个非零的int类型值即可
 //			System.exit(7);
-			return;
+//			return;
+		}catch(ArithmeticException e){
+			System.err.println("除数不能为0！");
+			e.printStackTrace();
+		}catch(Exception e){
+			System.out.println("出现了其它错误操作！");
+			e.printStackTrace();
 		}finally{
 			System.out.println("感谢使用本程序！");
 		}
 	}
 }
 
-//	对于try-catch-finally，try块中若有return，执行到return时不会直接退出方法，而会先执行finally，之后再执行return，退出方法。
-//	运行结果：
-//		请输入被除数：7
-//		请输入除数：2
-//		7/2=3
-//		感谢使用本程序！
-//	总结：不管return出现在try块中还是catch块中，遇到return时，都会先去执行finally块，再来执行return跳出方法。
-//		只有一种情况下不会执行finally，即System.exit(1)退出Java虚拟机。
+// 注意：不同的块（{...}）里可以出现同名的（局部）变量。
+// 多重catch的顺序为先写子类，后写父类。
+// 尽量去捕捉精确的异常，以便有精确的提示。
