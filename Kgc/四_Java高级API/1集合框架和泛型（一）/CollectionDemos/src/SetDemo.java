abc0a4bac38577484c7f8ea946c03b6dcb597dd4
Shane678 <shanewestlife@outlook.com>
2019-06-09 18:39:34 +0800

    Set接口

---
+++ Kgc/四_Java高级API/1集合框架和泛型（一）/CollectionDemos/src/SetDemo.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//测试Set的使用
+public class SetDemo {
+	public static void main(String[] args) {
+		NewsTitle title1 = new NewsTitle(1, "南京下雨了1", "admin");
+		NewsTitle title2 = new NewsTitle(2, "南京下雨了2", "admin");
+		NewsTitle title3 = new NewsTitle(3, "南京下雨了3", "admin");
+		NewsTitle title4 = new NewsTitle(4, "南京下雨了4", "admin");
+		NewsTitle title5 = new NewsTitle(5, "南京下雨了5", "admin");
+		
+		Set set = new HashSet(); // Set是接口，没法new，只能new它的实现类。
+		set.add(title1);
+		set.add(title2);
+		set.add(title3);
+		set.add(title4);
+		set.add(title5);
+		
+		System.out.println("新闻总记录数：" + set.size());
+		
+		/* 遍历无序的集合，可以使用的两个方式：
+		 * （1）增强型for
+		 * （2）iterator迭代器 */
+		
+		// 遍历每条新闻信息（增强型for方式）
+		// Set，结果顺序是无序的，和添加的顺序不是一样的。
+		for(Object obj : set){
+			NewsTitle title = (NewsTitle)obj;
+			System.out.println(title.getId() + "-" + title.getTitle());
+		}
+		
+		// 
+	}
+}
