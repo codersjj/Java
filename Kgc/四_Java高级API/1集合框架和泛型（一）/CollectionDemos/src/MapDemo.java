337d7521d4a8066ec0d4ae66ac3b970d8b3a2376
Shane678 <shanewestlife@outlook.com>
2019-06-11 21:58:16 +0800

    Map接口常用方法1

--- Kgc/四_Java高级API/1集合框架和泛型（一）/CollectionDemos/src/MapDemo.java
+++ Kgc/四_Java高级API/1集合框架和泛型（一）/CollectionDemos/src/MapDemo.java
@@ -1,5 +1,9 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+
+import sun.swing.text.CountingPrintable;
 
 // 存储国家英文简称-国家全称（键-值对）
 public class MapDemo {
@@ -11,7 +15,7 @@ public class MapDemo {
 		countries.put("FR", "法兰西共和国");
 		countries.put("US", "美利坚合众国");
 		
-		// 获取Map的元素个数
+		// 获取Map的元素组（对）数
 		System.out.println(countries.size());
 		
 		// 通过某个键获取对应的值
@@ -35,9 +39,28 @@ public class MapDemo {
 		System.out.println(countries);
 		
 		// 清空
-		countries.clear();
+		/*countries.clear();
 		if(countries.isEmpty()) {
 			System.out.println("Map数据已经清空");
+		}*/
+		
+		System.out.println("*******");
+		// 分别获取Map中的键和值
+		// （1）先获取到每个key，然后根据每个key拿到相应的value。
+		Set keys = countries.keySet();
+		// 方法一：使用增强型for拿到每个key
+		for(Object obj : keys){
+			String key = (String)obj;
+			String value = (String)countries.get(key);
+			System.out.println(key + "-" + value);
+		}
+		System.out.println("*******");
+		// 方法二：使用Iterator迭代器，拿到每个key
+		Iterator itor = keys.iterator();
+		while(itor.hasNext()){
+			String key = (String)itor.next();
+			String value = (String)countries.get(key);
+			System.out.println(key + "-" + value);
 		}
 	}
 }
