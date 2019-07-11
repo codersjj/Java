75531f7233f12063d99742e767c17087729075eb
Shane678 <shanewestlife@outlook.com>
2019-07-11 22:26:24 +0800

    字符串拆分及综合练习_局限条件下的split()

--- Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/strdemo/StrDemo6.java
+++ Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/strdemo/StrDemo6.java
@@ -1,9 +1,11 @@
 package cn.kgc.strdemo;
 
+import java.util.Scanner;
+
 // 字符串拆分方法：split()
 public class StrDemo6 {
 	public static void main(String[] args) {
-		String originalLyrics = "长亭外 古道边 芳草碧连天 晚风扶 柳笛声残 夕阳山外山";
+		/*String originalLyrics = "长亭外 古道边 芳草碧连天 晚风扶 柳笛声残 夕阳山外山";
 		String[] changedLyrics = new String[100];
 		
 		System.out.println("***原歌词格式***");
@@ -12,6 +14,22 @@ public class StrDemo6 {
 		System.out.println("***拆分后歌词格式***");
 		for (String lyrics : changedLyrics) {
 			System.out.println(lyrics);
+		}*/
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入一个带“-”的字符串（每个字符用“-”隔开，如：我-爱-你-中-国）：");
+		String info = input.next();
+		System.out.println("请输入您要查询的字符：");
+		String want = input.next();
+		String[] splitedInfos = info.split("-");
+		// 特定字符出现的次数
+		int count = 0;
+		for (int i = 0; i < splitedInfos.length; i++) {
+			if (splitedInfos[i].equals(want)) {
+				count++;
+			}
 		}
+		System.out.println("\"" + want + "\"在\"" + info + "\"中" + "出现的次数为：" + count + "次。");
+		
 	}
 }
