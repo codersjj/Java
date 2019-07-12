b7459dccb6c4f7761bb76ccaa21fb09d5a63a85c
Shane678 <shanewestlife@outlook.com>
2019-07-12 22:31:41 +0800

    StringBuffer类_insert()应用

--- Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/strdemo/Demo1.java
+++ Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/strdemo/Demo1.java
@@ -1,8 +1,10 @@
 package cn.kgc.strdemo;
 
+import java.util.Scanner;
+
 public class Demo1 {
 	public static void main(String[] args) {
-		// String --> StringBuffer
+/*		// String --> StringBuffer
 		StringBuffer s1 = new StringBuffer("Hello");
 		s1.append(" Peppa!");
 		System.out.println(s1); // Hello Peppa!
@@ -12,6 +14,15 @@ public class Demo1 {
 		
 		s1.insert(5, "!"); // Hello! Peppa!
 		System.out.println(s1);
+*/		
+		Scanner input = new Scanner(System.in);
+		System.out.println("ÇëÊäÈëÒ»´®Êý×Ö£º");
+		String num = input.next();
+		StringBuffer strBufNum = new StringBuffer(num);
+		for (int i = strBufNum.length() - 3; i > 0; i = i - 3) {
+			strBufNum.insert(i, ",");
+		}
+		System.out.println(strBufNum);
 	}
 
 }
