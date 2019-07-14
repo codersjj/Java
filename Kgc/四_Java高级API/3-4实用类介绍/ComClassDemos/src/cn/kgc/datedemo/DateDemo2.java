092c8f77eb0d82a4a2a7767fd694afd9a72c94cd
Shane678 <shanewestlife@outlook.com>
2019-07-14 12:20:49 +0800

    操作日期时间_add

--- Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/datedemo/DateDemo2.java
+++ Kgc/四_Java高级API/3-4实用类介绍/ComClassDemos/src/cn/kgc/datedemo/DateDemo2.java
@@ -8,5 +8,10 @@ public class DateDemo2 {
 		Calendar t = Calendar.getInstance(); // Calendar类是一个抽象类，没法new，需要通过它的一个静态方法getInstance()获取一个Calendar对象。
 		System.out.println(t.get(Calendar.YEAR) + "-" + (t.get(Calendar.MONTH) + 1) + "-" + t.get(Calendar.DAY_OF_MONTH));
 		System.out.println("今天是星期" + (t.get(Calendar.DAY_OF_WEEK) - 1));
+		
+		String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+		int week_index = t.get(Calendar.DAY_OF_WEEK) - 1;
+		String now = weeks[week_index];
+		System.out.println("今天是" + now);
 	}
 }
