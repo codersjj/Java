0507650761e2ec4bf63ca23320c12e5a9bab09b7
Shane678 <shanewestlife@outlook.com>
2019-07-17 22:17:20 +0800

    使用FileInputStream读取文件_可以读到的字节数

--- Kgc/四_Java高级API/6-7输入和输出处理/IOPrj/src/FileInputStreamDemo.java
+++ Kgc/四_Java高级API/6-7输入和输出处理/IOPrj/src/FileInputStreamDemo.java
@@ -10,11 +10,15 @@ public class FileInputStreamDemo {
 		try {
 			// step2：构造文件输入流FileInputStream对象
 			fis = new FileInputStream("d:\\JJSha\\text.txt");
-			// step3：读取文件内容数据，使用输入流的方法
+			// 可以读取到的字节数
+			System.out.println("可以读取到的字节数：" + fis.available());
+			// step3：读取文件内容数据，使用输入流的read()方法
 			int data;
 			while ((data = fis.read()) != -1) {
 				System.out.print((char)data);
 			}
+			// 流读到最后，可以读取到的字节数就为0了。
+			System.out.println("\n可以读取到的字节数：" + fis.available());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
