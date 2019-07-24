8750986fd6073c239a6f9e83e206390d5e256237
Shane678 <shanewestlife@outlook.com>
2019-07-25 07:21:15 +0800

    DOM4J解析实现增加新的手机品牌信息

--- Kgc/四_Java高级API/11-12XML/XML4JDemo/src/Dom4jDemo.java
+++ Kgc/四_Java高级API/11-12XML/XML4JDemo/src/Dom4jDemo.java
@@ -1,10 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
 
 public class Dom4jDemo {
@@ -36,10 +43,52 @@ public class Dom4jDemo {
 			}
 		}
 	}
+
+	// 增加新的手机品牌信息（相当于修改DOM树）
+	public void addNewPhone(){
+		// 获得XML根元素
+		Element root = document.getRootElement();
+		// 创建<Brand>
+		Element elBrand = root.addElement("Brand");
+		elBrand.addAttribute("name", "华为");
+		// 创建<Type name="HW123">
+		Element elType = elBrand.addElement("Type");
+		elType.addAttribute("name", "HW123");
+		
+		saveXML("newInfo.xml");
+	}
+	
+	// 保存修改到XML文件
+	public void saveXML(String path){
+		// OutputFormat相当于转换器
+		OutputFormat format = OutputFormat.createPrettyPrint();
+//		format.setEncoding("GBK");
+//		XMLWriter writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(path)));
+		format.setEncoding("GBK");
+		XMLWriter writer = null;
+		try {
+			writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(path), "GBK"), format);
+			writer.write(document);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 关闭输出流
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		Dom4jDemo domDemo = new Dom4jDemo();
 		domDemo.loadDocument();
+		domDemo.addNewPhone();
 		domDemo.showPhoneInfo();
 	}
 }
